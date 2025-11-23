ca53fd9d1b996d17d1f1d9f1af9450c296f2c1d4
Jie-Yan094 <jarita0904@gmail.com>
2025-11-23 16:06:26 +0000

    pages/00_home.py modified new file

--- pages/00_home.py
+++ pages/00_home.py
@@ -3,5 +3,6 @@ import solara
 @solara.component
 def Page():
     # 設定瀏覽器標籤頁的標題
-    solara.Title("我的 Solara WebGIS App")
+    solara.Title("蔡婕妍的 Solara WebGIS App")
     solara.Markdown("### **花蓮馬太鞍溪堰塞湖 WebGIS 應用程式**")
+    solara.image('https://row.github.com/Jie-Yan094/1119-Hualien/blob/8611836f4ef93b6cf959394c56ee8cac8cab78de/2.jpg')
