3480a8e3383c99f6736072884767e5959c786a21
Jie-Yan094 <jarita0904@gmail.com>
2025-11-23 16:10:58 +0000

    modified:   pages/00_home.py

--- pages/00_home.py
+++ pages/00_home.py
@@ -5,4 +5,4 @@ def Page():
     # 設定瀏覽器標籤頁的標題
     solara.Title("蔡婕妍的 Solara WebGIS App")
     solara.Markdown("### **花蓮馬太鞍溪堰塞湖 WebGIS 應用程式**")
-    solara.image('https://row.github.com/Jie-Yan094/1119-Hualien/blob/8611836f4ef93b6cf959394c56ee8cac8cab78de/2.jpg')
+   
